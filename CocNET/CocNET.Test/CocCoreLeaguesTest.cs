c766ae81d58af328bfca4cbcdd98bc884f7eba52
Kacper Drapała <kacdrapala@gmail.com>
2017-05-09 19:44:07 +0200

    Added token to CocCore

--- CocNET/CocNET.Test/CocCoreLeaguesTest.cs
+++ CocNET/CocNET.Test/CocCoreLeaguesTest.cs
@@ -11,12 +11,14 @@ namespace CocNET.Test
     [TestFixture]
     public class CocCoreLeaguesTest
     {
+        private const string TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU4NWEyYmVjLWZhMTEtNGM3Mi05YmQzLWU3ZWE5OWQ5NTM5YiIsImlhdCI6MTQ5NDM0NDA3NCwic3ViIjoiZGV2ZWxvcGVyLzhmY2VhNGY3LWE4NTctMDkyOS1hYTgyLTVkM2I1YThmOWRlNSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc4LjkuNzYuMTM3IiwiNTIuNDUuMTg1LjExNyIsIjUyLjU0LjMxLjExIiwiNTQuODcuMTQxLjI0NiIsIjU0Ljg3LjE4NS4zNSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.6_fP6KKlJ-11eR8u47PQ85E0CATi1hMGwGOPUT3D-hL4QfaWkbP5chtVKJFMkSJjZmGA6-dlTPLn01iZGAoBxw"; //Example token
+
         private ICocCoreLeagues LeaguesCore;
 
         [OneTimeSetUp]
         public void InitializeCore()
         {
-            LeaguesCore = CocCore.Instance.Container.Resolve<ICocCoreLeagues>();
+            LeaguesCore = CocCore.Instance(TOKEN).Container.Resolve<ICocCoreLeagues>();
         }
 
         [Test, Category("LeaguesTest")]
--- CocNET/CocNET/CocCore.cs
+++ CocNET/CocNET/CocCore.cs
@@ -10,36 +10,31 @@ namespace CocNET
     {
         private static object syncRoot = new Object();
 
-        private const string TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU4NWEyYmVjLWZhMTEtNGM3Mi05YmQzLWU3ZWE5OWQ5NTM5YiIsImlhdCI6MTQ5NDM0NDA3NCwic3ViIjoiZGV2ZWxvcGVyLzhmY2VhNGY3LWE4NTctMDkyOS1hYTgyLTVkM2I1YThmOWRlNSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc4LjkuNzYuMTM3IiwiNTIuNDUuMTg1LjExNyIsIjUyLjU0LjMxLjExIiwiNTQuODcuMTQxLjI0NiIsIjU0Ljg3LjE4NS4zNSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.6_fP6KKlJ-11eR8u47PQ85E0CATi1hMGwGOPUT3D-hL4QfaWkbP5chtVKJFMkSJjZmGA6-dlTPLn01iZGAoBxw"; //Example token
-
         public Container Container { get; set; }
         private static CocCore instance;
-        public static CocCore Instance
+        public static CocCore Instance(string token)
         {
-            get
+            lock (syncRoot)
             {
-                lock(syncRoot)
+                if (instance == null)
                 {
-                    if (instance == null)
-                    {
-                        instance = new CocCore();
-                    }
+                    instance = new CocCore(token);
                 }
-                return instance;
             }
+            return instance;
         }
         /// <summary>
         /// Initialize your core.
         /// </summary>
-        private CocCore()
+        private CocCore(string token)
         {
-            if (string.IsNullOrEmpty(TOKEN))
+            if (string.IsNullOrEmpty(token))
             {
                 throw new ArgumentException("Token is nullable or empty.");
             }
             Container = new Funq.Container();
 
-            Container.Register<Request>("Request", new Request(TOKEN));
+            Container.Register<Request>("Request", new Request(token));
 
             Container.Register<ICocCoreClans>(new CocCoreClans(Container.ResolveNamed<Request>("Request")));
             Container.Register<ICocCoreLocations>(new CocCoreLocations(Container.ResolveNamed<Request>("Request")));
